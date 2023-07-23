4cd89a9e121f27e93a29bb6e8df6f22140606530
basemhemaid <basemhemaid2000@gmail.com>
2023-07-23 16:45:41 +0300

    create Movies website

--- src/Pages/Movie-detail/movieDetail.js
+++ src/Pages/Movie-detail/movieDetail.js
@@ -49,18 +49,6 @@ export default function MoviesDetail()
                     </div>
                 </div>
             </div>
-
-            {/* <div className="row row-cols-1 row-cols-md-3 m-2 g-4">
-                    <div key={products.id} className="col">
-                        <div className="card">
-                            <img src={'https://image.tmdb.org/t/p/original' + products.poster_path} className="card-img-top" alt="..." />
-                            <div className="card-body">
-                                <h5 className="card-title">{products.title}</h5>
-                                <p className="card-text">{products.overview}</p>
-                            </div>
-                        </div>
-                    </div>
-            </div> */}
         </>
     );
 }
